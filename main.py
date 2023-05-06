e3d65c2f8bfaf3be259b8348a9f69ad606822eb8
diegoag30 <diego.pablo.aguilar@gmail.com>
2023-05-06 02:51:59 -0300

    layout: added assets and change character

--- main.py
+++ main.py
@@ -1,44 +1,46 @@
-import pygame
 import os
 
+import pygame
 from sqlalchemy import true
+
+from level import Level
 from settings import *
 from tile import Tile
-from level import Level
 
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-BACKGROUND_COLOR = (0,0,0)
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+BACKGROUND_COLOR = (0, 0, 0)
 pygame.display.set_caption("Bienvenido al Juego")
-level = Level(level_map,WIN)
+level = Level(level_map, WIN)
 
 FPS = 60
-IMAGE_PATH = os.path.join('Assets','Hero Knight','Sprites','HeroKnight','Idle')
-FIRST_IMAGE = os.path.join(IMAGE_PATH,'HeroKnight_Idle_0.png')
+IMAGE_PATH = os.path.join('assets', 'character', 'wind_hashashin', 'sprites', 'idle')
+FIRST_IMAGE = os.path.join(IMAGE_PATH, 'idle_1.png')
 MAIN_CHARACTER = pygame.image.load(FIRST_IMAGE)
-MAIN_CHARACTER_WIDHT, MAIN_CHARACTER_HEIGHT = 120,100
+MAIN_CHARACTER_WIDHT, MAIN_CHARACTER_HEIGHT = 120, 100
 MAIN_CHARACTER = pygame.transform.scale(MAIN_CHARACTER, (MAIN_CHARACTER_WIDHT, MAIN_CHARACTER_HEIGHT))
 VEL = 2
 CHAR_STAND = [
-    os.path.join(IMAGE_PATH,'HeroKnight_Idle_0.png'),
-    os.path.join(IMAGE_PATH,'HeroKnight_Idle_1.png'),
-    os.path.join(IMAGE_PATH,'HeroKnight_Idle_2.png'),
-    os.path.join(IMAGE_PATH,'HeroKnight_Idle_3.png'),
-    os.path.join(IMAGE_PATH,'HeroKnight_Idle_4.png'),
-    os.path.join(IMAGE_PATH,'HeroKnight_Idle_5.png'),
-    os.path.join(IMAGE_PATH,'HeroKnight_Idle_6.png'),
-    os.path.join(IMAGE_PATH,'HeroKnight_Idle_7.png')
-    ]
+    os.path.join(IMAGE_PATH, 'idle_1.png'),
+    os.path.join(IMAGE_PATH, 'idle_2.png'),
+    os.path.join(IMAGE_PATH, 'idle_3.png'),
+    os.path.join(IMAGE_PATH, 'idle_4.png'),
+    os.path.join(IMAGE_PATH, 'idle_5.png'),
+    os.path.join(IMAGE_PATH, 'idle_6.png'),
+    os.path.join(IMAGE_PATH, 'idle_7.png'),
+    os.path.join(IMAGE_PATH, 'idle_8.png')
+]
+
 
 def draw_window():
     WIN.fill((BACKGROUND_COLOR))
-    #WIN.blit(MAIN_CHARACTER,(player.x,player.y))
+    # WIN.blit(MAIN_CHARACTER,(player.x,player.y))
     pygame.display.update()
-  
+
 
 def main():
     is_jump = False
     jump_count = 10
-    #player_one = pygame.Rect(20,400,MAIN_CHARACTER_WIDHT,MAIN_CHARACTER_HEIGHT)
+    # player_one = pygame.Rect(20,400,MAIN_CHARACTER_WIDHT,MAIN_CHARACTER_HEIGHT)
     clock = pygame.time.Clock()
     run = True
     # MAIN LOOP
@@ -51,11 +53,12 @@ def main():
         WIN.fill((BACKGROUND_COLOR))
         level.run()
 
-        #draw_window()
+        # draw_window()
         pygame.display.update()
         clock.tick(FPS)
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,24 +1,27 @@
-from pickle import TRUE
-import pygame
 import os
+from pickle import TRUE
+
+import pygame
+
 from support import import_folder
 
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos) :
+    def __init__(self, pos):
         super().__init__()
         self.import_character_assets()
         self.frame_index = 0
         self.animation_speed = 0.15
         self.image = self.animations['idle'][self.frame_index]
-        self.rect = self.image.get_rect(topleft = pos)
-        self.direction = pygame.math.Vector2(0,0)
+        self.rect = self.image.get_rect(topleft=pos)
+        self.direction = pygame.math.Vector2(0, 0)
 
-        #player movement
+        # player movement
         self.speed = 4
         self.gravity = 0.8
-        self.jump_speed = -16 #Negativo para que sea hacia arriba
+        self.jump_speed = -16  # Negativo para que sea hacia arriba
 
-        #player status
+        # player status
         self.status = 'idle'
         self.is_facing_right = True
         self.on_ground = True
@@ -28,8 +31,8 @@ class Player(pygame.sprite.Sprite):
         self.remaining_jump = 4
 
     def import_character_assets(self):
-        character_path = os.path.join('Assets','character')
-        self.animations = {'idle':[], 'run':[], 'jump':[], 'fall':[]}
+        character_path = os.path.join('assets', 'character', 'wind_hashashin', 'sprites')
+        self.animations = {'idle': [], 'run': [], 'jump': [], 'fall': []}
 
         for animation in self.animations.keys():
             full_path = os.path.join(character_path, animation)
@@ -37,19 +40,18 @@ class Player(pygame.sprite.Sprite):
 
     def get_input(self):
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_a] : #LEFT
+        if keys_pressed[pygame.K_a]:  # LEFT
             self.direction.x = -1
             self.is_facing_right = False
-        elif keys_pressed[pygame.K_d] : #RIGHT
+        elif keys_pressed[pygame.K_d]:  # RIGHT
             self.direction.x = 1
             self.is_facing_right = True
         else:
             self.direction.x = 0
 
-        if keys_pressed[pygame.K_SPACE] and self.remaining_jump > 0: #SPACE
+        if keys_pressed[pygame.K_SPACE] and self.remaining_jump > 0:  # SPACE
             self.jump()
-            #self.remaining_jump = self.remaining_jump - 1
-
+            # self.remaining_jump = self.remaining_jump - 1
 
     def get_status(self):
         if self.direction.y < 0:
@@ -57,7 +59,7 @@ class Player(pygame.sprite.Sprite):
         elif self.direction.y > 1:
             self.status = 'fall'
         else:
-            if self.direction.x != 0 :
+            if self.direction.x != 0:
                 self.status = 'run'
             else:
                 self.status = 'idle'
@@ -65,36 +67,35 @@ class Player(pygame.sprite.Sprite):
     def jump(self):
         self.direction.y = self.jump_speed
 
-
     def apply_gravity(self):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
 
     def animate(self):
-        animation = self.animations[self.status]
+        pass
+        # animation = self.animations[self.status]
 
-        #Loop over the frame index
-        self.frame_index += self.animation_speed
-        if self.frame_index >= len(animation):
-            self.frame_index = 0
+        # # Loop over the frame index
+        # self.frame_index += self.animation_speed
+        # if self.frame_index >= len(animation):
+        #     self.frame_index = 0
 
-        image = animation[int(self.frame_index)]
-        if self.is_facing_right:
-            self.image = image
-        else:
-            flipped_image = pygame.transform.flip(image, True, False)
-            self.image = flipped_image
+        # image = animation[int(self.frame_index)]
+        # if self.is_facing_right:
+        #     self.image = image
+        # else:
+        #     flipped_image = pygame.transform.flip(image, True, False)
+        #     self.image = flipped_image
 
-        #set the rectangle
-        if self.on_ground:
-            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
-        elif self.on_ceiling:
-            self.rect = self.image.get_rect(midtop = self.rect.midtop)
-        else:
-            self.rect = self.image.get_rect(center = self.rect.center)
+        # # set the rectangle
+        # if self.on_ground:
+        #     self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
+        # elif self.on_ceiling:
+        #     self.rect = self.image.get_rect(midtop=self.rect.midtop)
+        # else:
+        #     self.rect = self.image.get_rect(center=self.rect.center)
 
     def update(self):
         self.get_input()
         self.get_status()
         self.animate()
-
